0e103ee7e22c6611601f4ae960434b6db61447d2
Jens Jonassen <jonassenjens@gmail.com>
2024-10-20 20:58:37 +0200

    initial commit

---
+++ receiver/src/test/java/com/jens/receiver/ReceiverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jens.receiver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceiverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
